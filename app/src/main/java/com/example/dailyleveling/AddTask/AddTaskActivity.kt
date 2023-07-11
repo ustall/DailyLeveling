e6983b7e5ad783b9b702ed3bb219a6b150cf2023
Ustal <umnik.ya5@gmail.com>
2023-07-11 06:36:34 +0300

    11.07.2023 2

--- app/src/main/java/com/example/dailyleveling/AddTask/AddTaskActivity.kt
+++ app/src/main/java/com/example/dailyleveling/AddTask/AddTaskActivity.kt
@@ -1,12 +1,28 @@
 package com.example.dailyleveling.AddTask
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.dailyleveling.MainActivity
+import com.example.dailyleveling.MainScreen.TaskAdapter
 import com.example.dailyleveling.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class AddTaskActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_task)
+        supportActionBar?.hide()
+        addTask()
+    }
+    private fun addTask() {
+        val addButton = findViewById<CardView>(R.id.button_type)
+        addButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/dailyleveling/AddTask/buttonsRvAdapter.kt
+++
@@ -1,4 +0,0 @@
-package com.example.dailyleveling.AddTask
-
-class buttonsRvAdapter {
-}
